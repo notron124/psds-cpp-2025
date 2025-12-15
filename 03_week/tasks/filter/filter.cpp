8504d0e3b1f5ee1343e641783682a5a2effbe7d2
Radiy <notron12460@gmail.com>
2025-12-15 17:31:29 +0500

    WIP task filter

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,17 @@
 #include <stdexcept>
 
+typedef bool (*Predicate)(int);
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+void Filter(std::vector<int> &v, Predicate predicateFunc) { 
+    if (v.empty() || predicateFunc == nullptr) {
+        return;
+    }
+    
+    for (auto it = v.begin(); it != v.end();) {
+        if (predicateFunc(*it)) {
+            it = v.erase(it);
+        } else {
+            ++it;
+        }
+    }
 }
