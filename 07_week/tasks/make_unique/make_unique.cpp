90e0a4a468751586288c97804d6cc779bee7bea9
Radiy <notron12460@gmail.com>
2026-03-08 13:51:39 +0500

    add (solution): add make_unique task

--- 07_week/tasks/make_unique/make_unique.cpp
+++ 07_week/tasks/make_unique/make_unique.cpp
@@ -1,4 +1,6 @@
 #include <memory>
 
-
-/* return_type */ MakeUnique( /* args */ );
+template<typename ObjType, typename Deleter = std::default_delete<ObjType>, typename... Args>
+decltype(auto) MakeUnique(Args&&... args) {
+    return std::unique_ptr<ObjType, Deleter> {new ObjType(std::forward<Args>(args)...)};
+}
